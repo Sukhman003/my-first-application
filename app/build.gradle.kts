2f23ad98e0f3efe0fa4a30a4834f3f347dafb913
KapilSaini <kapilworkpro@gmail.com>
2024-09-14 14:50:27 -0400

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,8 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
-
+binddFeatures{ this: ApplicationBindFeatures
+    y.iu.unding=true
 dependencies {
 
     implementation(libs.appcompat)
--- app/src/main/java/com/example/trial2/MainActivity.java
+++ app/src/main/java/com/example/trial2/MainActivity.java
@@ -2,24 +2,35 @@
 package com.example.trial2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsCompat;;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    Button btn1;
+            Button btn2;
+            TextView txt;
+     @Override
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+         btn1=findViewById(R.id.btn1);
+
+         btn2=findViewById(R.id.btn2);
+         txt=findViewById(R.id.txt1);
+                 btn1.setOnClickListener(view ->  {
+             CharSequence text = "Hello toast from Kapil!";
+             int duration = Toast.LENGTH_LONG;
+             Toast toast = Toast.makeText(this, text, duration);
+                     toast. show() ;
+
         });
     }
 }
